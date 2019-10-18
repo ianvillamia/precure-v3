4588dd4b1fde7b98d7624a6c5328fb81482bffc7
StarbrandedSentry <hierarchhero@gmail.com>
2019-10-18 22:14:28 +0800

    transactions almost complete 😀😀😀

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { map, takeWhile } from 'rxjs/operators';
 import { AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
 import { Transaction } from '../models/transaction.model';
+import { TransactionService } from '../shared/transaction.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -16,12 +17,24 @@ export class DashboardComponent implements OnInit {
 
   //gIHQ7ONtv9uw4fsJzRni
 
-  constructor(private afs: AngularFirestore) { 
+  constructor(private afs: AngularFirestore, public transactionServ: TransactionService) { 
     
   }
 
   ngOnInit() {
+    this.transactionCol = this.afs.collection('users/gIHQ7ONtv9uw4fsJzRni/transactions', ref => ref.orderBy('delivery_date', 'desc'));
+    this.activeTransactions = this.transactionCol.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Transaction;
+        data.id = a.payload.doc.id;
+        return data;
+      }))
+    );
 
+    this.activeTransactions.subscribe(transactions => {
+      this.transactionServ.activeTransactions = transactions;
+      console.log(transactions);
+    });
   }
 
 }
