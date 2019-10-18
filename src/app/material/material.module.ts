a06bcda9359b01b7ba0971f96ae35b4937cda427
Ciaossuuu <55319885+Ciaossuuu@users.noreply.github.com>
2019-10-18 16:15:59 +0800

    create purchase order

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule} from '@angular/forms';
-import {
+import { 
   MatButtonModule,
   MatInputModule,
   MatFormFieldModule,
-  MatSidenavModule
+  MatSidenavModule,
+  MatDatepickerModule
 
 } from '@angular/material';
 
 
 const  MaterialComponents = [
-  MatButtonModule,MatFormFieldModule,  MatInputModule,MatSidenavModule,FormsModule];
+  MatButtonModule,MatFormFieldModule,  MatInputModule,MatSidenavModule,FormsModule, MatDatepickerModule];
 @NgModule({
   imports: [MaterialComponents],
   exports:[MaterialComponents]
