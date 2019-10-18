7dc0a0bbfae285163ae3d89522659ae633d847be
Ciaossuuu <55319885+Ciaossuuu@users.noreply.github.com>
2019-10-18 14:16:11 +0800

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {DashboardComponent} from './home/dashboard/dashboard.component'
+import { DashboardComponent } from './home/dashboard/dashboard.component'
+import { PurchasingComponent } from './home/purchasing/purchasing.component'
+import { BudgetsComponent } from './home/budgets/budgets.component'
+import { ReportsComponent } from './home/reports/reports.component'
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path:'',component:DashboardComponent},
-  {path:'dashboard',component:DashboardComponent}
+  {path:'', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent, children:[
+    {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+    {path:'dashboard', component: DashboardComponent},
+    {path:'purchase-order', component: PurchasingComponent},
+    {path:'budget', component: BudgetsComponent},
+    {path:'report', component: ReportsComponent}
+  ]}
+  
+  
+  
 ];
 
 @NgModule({
