df872267c0c91518c4a8c6ce478371fa75a3a12d
StarbrandedSentry <hierarchhero@gmail.com>
2019-10-19 08:46:40 +0800

    more spinners ⚓

--- src/app/home/budgets/budgets.component.ts
+++ src/app/home/budgets/budgets.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, takeWhile } from 'rxjs/operators';
 import { AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
-import { Budget } from '../models/budget.model';
+
 import { TransactionService } from '../shared/transaction.service';
 
 @Component({
@@ -11,24 +11,12 @@ import { TransactionService } from '../shared/transaction.service';
   styleUrls: ['./budgets.component.scss']
 })
 export class BudgetsComponent implements OnInit {
-  budgets: Observable<Budget[]>;
-  budgetCol: AngularFirestoreCollection<Budget>;
+  
 
-  constructor(private afs: AngularFirestore, public transactionServ: TransactionService) { }
+  constructor(public transactionServ: TransactionService) { }
 
   ngOnInit() {
-    this.budgetCol = this.afs.collection('users/gIHQ7ONtv9uw4fsJzRni/budgets', ref => ref.orderBy('name', 'desc'));
-    this.budgets = this.budgetCol.snapshotChanges().pipe(
-      map(actions => actions.map(a => {
-        const data = a.payload.doc.data() as Budget;
-        data.id = a.payload.doc.id;
-        return data;
-      }))
-    );
-
-    this.budgets.subscribe(budget => {
-      this.transactionServ.budgets = budget;
-    });
+    
   }
 
 }
--- src/app/home/shared/transaction.service.ts
+++ src/app/home/shared/transaction.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Transaction } from '../models/transaction.model';
 import { Budget } from '../models/budget.model';
+import { Location, ItemCategory } from '../models/arrays.model';
+import { Observable } from 'rxjs';
+import { map, takeWhile } from 'rxjs/operators';
+import { AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +12,23 @@ import { Budget } from '../models/budget.model';
 export class TransactionService {
   activeTransactions: Transaction[];
   budgets: Budget[];
+  locations: Location[];
+  budgetsO: Observable<Budget[]>;
+  budgetCol: AngularFirestoreCollection<Budget>;
 
-  constructor() { 
+  constructor(private afs: AngularFirestore) { 
+    this.budgetCol = this.afs.collection('users/gIHQ7ONtv9uw4fsJzRni/budgets', ref => ref.orderBy('name', 'desc'));
+    this.budgetsO = this.budgetCol.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Budget;
+        data.id = a.payload.doc.id;
+        return data;
+      }))
+    );
 
+    this.budgetsO.subscribe(budget => {
+      this.budgets = budget;
+    });
   }
 
 
